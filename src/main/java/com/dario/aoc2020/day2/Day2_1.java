f57485c7a2f1018120d2a2f877096b0efe17e1ce
Dario Pasquantonio <dario.pasquantonio@klarna.com>
2020-12-02 06:19:13 +0100

    Day two

--- src/main/java/com/dario/aoc2020/day2/Day2_1.java
+++ src/main/java/com/dario/aoc2020/day2/Day2_1.java
@@ -1,7 +1,29 @@
 package com.dario.aoc2020.day2;
 
+import com.dario.aoc2020.utils.FileReader;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class Day2_1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<String> input = FileReader.read("day2/input.txt");
+        int cont = 0;
+        for (String line: input) {
+            String[] tokens = line.split(" ");
+            String times = tokens[0];
+            int min = Integer.parseInt(times.split("-")[0]);
+            int max = Integer.parseInt(times.split("-")[1]);
+            String letter = tokens[1].replace(":", "");
+            String password = tokens[2];
+            int occurrences = StringUtils.countMatches(password, letter);
+            if (occurrences >= min && occurrences <= max) {
+                cont++;
+            }
+        }
+        System.out.println(cont);
     }
 }
--- src/main/java/com/dario/aoc2020/day2/Day2_2.java
+++ src/main/java/com/dario/aoc2020/day2/Day2_2.java
@@ -1,6 +1,29 @@
 package com.dario.aoc2020.day2;
 
+import com.dario.aoc2020.utils.FileReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class Day2_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<String> input = FileReader.read("day2/input.txt");
+        int cont = 0;
+        for (String line: input) {
+            String[] tokens = line.split(" ");
+            String times = tokens[0];
+            int pos1 = Integer.parseInt(times.split("-")[0]) - 1;
+            int pos2 = Integer.parseInt(times.split("-")[1]) - 1;
+            String letter = tokens[1].replace(":", "");
+            String password = tokens[2];
+            boolean contains1 = password.charAt(pos1) == letter.charAt(0);
+            boolean contains2 = password.charAt(pos2) == letter.charAt(0);
+
+            if (contains1 ^ contains2) {
+                cont++;
+            }
+        }
+        System.out.println(cont);
     }
 }
