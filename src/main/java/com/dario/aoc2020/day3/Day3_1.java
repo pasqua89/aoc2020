ce19bd6c9ddf2e902dce3512b959b0d09181d049
Dario Pasquantonio <dario.pasquantonio@klarna.com>
2020-12-04 15:24:49 +0100

    Day4 cleanup

--- src/main/java/com/dario/aoc2020/day3/Day3_1.java
+++ src/main/java/com/dario/aoc2020/day3/Day3_1.java
@@ -1,38 +1,37 @@
-//package com.dario.aoc2020.day3;
-//
-//import com.dario.aoc2020.utils.FileReader;
-//import org.apache.commons.lang3.StringUtils;
-//
-//import java.io.IOException;
-//import java.net.URISyntaxException;
-//import java.util.List;
-//
-//public class Day3_1 {
-//
-//    public static void main(String[] args) throws IOException, URISyntaxException {
-//        List<String> input = FileReader.read("day3/input.txt");
-//        int reps = 50;
-//        String[][] grid  = new String[323][31 * reps];
-//        for (int t=0;t<reps;t++) {
-//            for (int i = 0; i < input.size(); i++) {
-//                for (int j = 0; j < input.get(0).length(); j++) {
-//                    grid[i][j + (t * 31)] = input.get(i).charAt(j) + "";
-//                }
-//            }
-//        }
-//
-//        int cont = 0;
-//        int posi = 0;
-//        int posj = 0;
-//        while (posi < 323) {
-//            String s = grid[posi][posj];
-//            if (s.equals("#")) {
-//                cont++;
-//            }
-//            posi+=1;
-//            posj+=3;
-//        }
-//
-//        System.out.println(cont);
-//    }
-//}
+package com.dario.aoc2020.day3;
+
+import com.dario.aoc2020.utils.FileReader;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Day3_1 {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<String> input = FileReader.read("day3/input.txt");
+        int reps = 50;
+        String[][] grid  = new String[323][31 * reps];
+        for (int t=0;t<reps;t++) {
+            for (int i = 0; i < input.size(); i++) {
+                for (int j = 0; j < input.get(0).length(); j++) {
+                    grid[i][j + (t * 31)] = input.get(i).charAt(j) + "";
+                }
+            }
+        }
+
+        int cont = 0;
+        int posi = 0;
+        int posj = 0;
+        while (posi < 323) {
+            String s = grid[posi][posj];
+            if (s.equals("#")) {
+                cont++;
+            }
+            posi+=1;
+            posj+=3;
+        }
+
+        System.out.println(cont);
+    }
+}
